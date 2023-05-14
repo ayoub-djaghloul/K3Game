e57d3c299b8c064792b1e964a386d26619c3fdf6
AyoubDjaghloul <ayoubdj19@gmail.com>
2023-05-14 22:58:30 +0200

    réglage RandomAI

--- src/AI/RandomAI.java
+++ src/AI/RandomAI.java
@@ -6,18 +6,19 @@ import Model.Pyramide;
 import Model.Table2D;
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 
 public class RandomAI {
 
 
-    ImageIcon BEIGE = new ImageIcon("../sources/Images/BEIGE.png");
-    ImageIcon BLANC = new ImageIcon("../sources/Images/BLANC.png");
-    ImageIcon BLEU = new ImageIcon("../sources/Images/BLEU.png");
-    ImageIcon JAUNE = new ImageIcon("../sources/Images/JAUNE.png");
-    ImageIcon NOIR = new ImageIcon("../sources/Images/NOIR.png");
-    ImageIcon ROUGE = new ImageIcon("../sources/Images/ROUGE.png");
-    ImageIcon VERT = new ImageIcon("../sources/Images/VERT.png");
+    ImageIcon BEIGE = new ImageIcon("sources/Images/BEIGE.png");
+    ImageIcon BLANC = new ImageIcon("sources/Images/BLANC.png");
+    ImageIcon BLEU = new ImageIcon("sources/Images/BLEU.png");
+    ImageIcon JAUNE = new ImageIcon("sources/Images/JAUNE.png");
+    ImageIcon NOIR = new ImageIcon("sources/Images/NOIR.png");
+    ImageIcon ROUGE = new ImageIcon("sources/Images/ROUGE.png");
+    ImageIcon VERT = new ImageIcon("sources/Images/VERT.png");
 
     public RandomAI(Pyramide p2Pyramide, Table2D baseK3) {
         RandomPyramid(p2Pyramide, baseK3);
@@ -29,13 +30,16 @@ public class RandomAI {
         Pion pionSource = null;
         for(int i = 0; i<pyramide.getHight(); i++){
             for (int j=0 ; j<=i;j++){
+                //liste pour les indices du couleur qui ne sont pas null
+                ArrayList<Integer> listdescouleurnonnul = new ArrayList<>();
+                for (int k = 0; k < colors.length; k++) {
+                    if(colors[k] != 0)
+                        listdescouleurnonnul.add(k);
+                }
+                    index = (int) (Math.random() * listdescouleurnonnul.size());
+                colors[listdescouleurnonnul.get(index)]--;
 
-                do{
-                    index = (int) (Math.random() * (colors.length - 1));
-                }while(colors[index] == 0);
-                colors[index]--;
-
-                switch (index){
+                switch (listdescouleurnonnul.get(index)){
                     case 0 -> pionSource = new Pion(CouleurPion.ROUGE, Pion.TypePion.COLORED, ROUGE, i, j);
                     case 1 -> pionSource = new Pion(CouleurPion.BLEU, Pion.TypePion.COLORED, BLEU, i, j);
                     case 2 -> pionSource = new Pion(CouleurPion.VERT, Pion.TypePion.COLORED, VERT, i, j);
