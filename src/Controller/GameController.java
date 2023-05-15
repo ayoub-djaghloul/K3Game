33fe9d287ae4c9c881fe1014906d391ec9c0d79a
Mekaoussi Mohamed Fares <62190207+faresmk@users.noreply.github.com>
2023-05-14 18:27:17 -1200

    gamecontroller

--- src/Controller/GameController.java
+++ src/Controller/GameController.java
@@ -24,21 +24,45 @@ public class GameController {
             destination.replacePion(source);
             System.out.println("Deplacement effectué parceque la source est beige");
             destination.setAccessible(false);
+            source.setAccessible(true);
             return true;
         }else if (CSource==pionfils1.getCouleur()&&CSource==pionfils2.getCouleur()) {
             destination.replacePion(source);
             System.out.println("Deplacement effectué mais avec pénalité");
             destination.setAccessible(false);
+            source.setAccessible(true);
             return true;
         } else if (pionfils1.getCouleur() == CSource || pionfils2.getCouleur() == CSource) {
             System.out.println("y a un fils qui a la meme couleur que la source");
             destination.replacePion(source);
             destination.setAccessible(false);
+            source.setAccessible(true);
             return true;
         }  else {
             System.out.println("Deplacement impossible");
             return false;
         }
     }}
+
+    public boolean testAvantDeplacement(Pion Source, Pyramide pyramide){
+        int x = Source.getX();
+        int y = Source.getY();
+        if (x==0){
+            return true;
+        } else if (y==0) {
+            if (pyramide.getPion(x-1,y).estAccessible()){
+                return true;
+            }else
+                return false;
+        }else if (y==x) {
+            if (pyramide.getPion(x - 1, y - 1).estAccessible()) {
+                return true;
+            } else
+                return false;
+        }else if (pyramide.getPion(x-1,y).estAccessible() && pyramide.getPion(x-1,y+1).estAccessible()){
+            return true;
+        }else
+            return false;
+        }
 
 }
